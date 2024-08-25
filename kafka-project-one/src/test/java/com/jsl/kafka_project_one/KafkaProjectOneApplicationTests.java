1af7f1af0ab63960f5fa51cf69f2379127485920
Justice Sam <fjaccobian@gmail.com>
2024-08-25 10:45:31 -0400

    Initial commit

---
+++ kafka-project-one/src/test/java/com/jsl/kafka_project_one/KafkaProjectOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsl.kafka_project_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProjectOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
